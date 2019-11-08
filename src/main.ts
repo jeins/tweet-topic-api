c18bcc5e43dc654c4e3df71fd79f142b4048e7bd
Muhammad Juan Akbar <mail@mjuan.me>
2019-11-08 21:12:41 +0100

    add env file, to manage global variable

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { config } from 'dotenv';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  config();
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.listen(process.env.PORT);
 }
 bootstrap();
